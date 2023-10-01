1be68cc65d779552f3a3e0026a814fafeaae310e
Uzair <uzairahmed2102005@gmail.com>
2023-10-01 19:26:37 +0530

    begin third module

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
-
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges= {
     "january":'Fast everyday for 6 hours',
@@ -16,9 +16,31 @@ monthly_challenges= {
     "november":'Learn Django for 20 mins everyday',
     "december":"Prepare next year"
 }
+
+
+
 # Create your views here.
+
+def index(request):
+    list_items = ""
+    months=list(monthly_challenges.keys())
+    for month in months:
+        capitlized_month=month.capitalize()
+        month_path = reverse("monthly-challenges",args=[month])
+        list_items+=f"<li><a href=\"{month_path}\">{capitlized_month}</a></li>"
+    response_data=f"<ul>{list_items}</ul>"
+
+    return HttpResponse(response_data)
+
+
+
 def monthly_challenges_by_number(request,month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+    redirect_month = months[month-1]
+    redirect_path = reverse("monthly-challenges",args =[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request,month):
     try:
